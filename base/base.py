12a49ef7a49310a3a3e29ec4a3d51a3e28f729dd
vothin <zy757161350@qq.com>
2020-06-04 17:37:08 +0800

    底层模块

--- base/base.py
+++ base/base.py
@@ -5,4 +5,16 @@
 
 # ********************************************************
 
+from common.read_config import ReadConfig
 
+
+class Base():
+
+    # 初始化读取配置文件url.ini
+    def __init__(self):
+        self.c = ReadConfig()
+        self.suffix = ''  # section参数
+        self.url = ''  # url地址
+        self.token = ''  # token参数
+        self.uid = ''  # uid参数
+        self.headers = None  # headers参数
