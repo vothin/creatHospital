4d34d1464f8d467a43e4a61c2d57b2711baeea33
vothin <zy757161350@qq.com>
2020-06-04 18:03:29 +0800

    底层模块

--- base/base.py
+++ base/base.py
@@ -5,11 +5,24 @@
 
 # ********************************************************
 
+import pymysql
+from base.global_path import mysql_path
 from common.read_yaml import ReadYaml
+
 
 
 class Base():
 
     # 初始化读取配置文件url.ini
-    def __init__(self):
-        pass
+    def __init__(self, sec_name):
+        self.mysql = ReadYaml(mysql_path)
+        self.mysql_conf = self.mysql.getValue(sec_name)
+
+        self.connect = pymysql.Connect(
+            host = self.mysql_conf['url'],
+            port = self.mysql_conf['port'],
+            user = str(self.mysql_conf['username']),
+            passwd = str(self.mysql_conf['password']),
+            db = self.mysql_conf['db'],
+            charset = 'utf8'
+        )
