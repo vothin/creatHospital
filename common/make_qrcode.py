c9466b79c42939829bcb045571be997817d2d301
vothin <zy757161350@qq.com>
2020-06-05 12:16:37 +0800

    制作二维码

---
+++ common/make_qrcode.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/5 12:12
+# @Author : Vothin
+# @File : make_qrcode.py
+
+# ********************************************************
+
+import qrcode
+from base.global_path import image_path
+from common.record_log import logs
+
+class MakeQRCode():
+
+    def __init__(self, data):
+
+        qr = qrcode.QRCode(
+                version=4,          #生成二维码尺寸的大小 1-40  1:21*21（21+(n-1)*4）
+                error_correction=qrcode.constants.ERROR_CORRECT_M,      #L:7% M:15% Q:25% H:30%
+                box_size=10,        #每个格子的像素大小
+                border=2            #边框的格子宽度大小
+            )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image()
+        img.save(image_path)
+        logs.info('制作科室二维码')
+
+
