e279dc6a5e990586d27a043a081a1783009a77e8
vothin <zy757161350@qq.com>
2020-06-05 17:35:02 +0800

    执行层

--- main/main.py
+++ main/main.py
@@ -5,3 +5,28 @@
 
 # ********************************************************
 
+from common.dml_mysql import DML_Mysql
+from common.write_excel import WriteExcel
+from common.record_log import logs
+
+class Main(DML_Mysql):
+
+    def main(self, parent_ip):
+
+        # 查询数据
+        logs.info('查询shop_id为%s下的科室' % parent_ip)
+        data = self.selectData(parent_ip)
+
+        # 写入到Excel表格
+        logs.info('写入到excel表格')
+        w = WriteExcel()
+        w.writeExcel(data)
+        w.closeExcel()
+
+if __name__ == '__main__':
+    m = Main('mysql_39')
+    m.main('200')
+
+
+
+
