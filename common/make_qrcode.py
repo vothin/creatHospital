48d2c9c25ff276eb9546895b20e550b871bf49ee
vothin <zy757161350@qq.com>
2020-06-05 15:26:46 +0800

    制作二维码

--- common/make_qrcode.py
+++ common/make_qrcode.py
@@ -14,10 +14,10 @@ class MakeQRCode():
     def __init__(self, data):
 
         qr = qrcode.QRCode(
-                version=4,          #生成二维码尺寸的大小 1-40  1:21*21（21+(n-1)*4）
-                error_correction=qrcode.constants.ERROR_CORRECT_M,      #L:7% M:15% Q:25% H:30%
-                box_size=10,        #每个格子的像素大小
-                border=2            #边框的格子宽度大小
+                version=1,          #生成二维码尺寸的大小 1-40  1:21*21（21+(n-1)*4）
+                error_correction=qrcode.constants.ERROR_CORRECT_L,      #L:7% M:15% Q:25% H:30%
+                box_size=6,        #每个格子的像素大小
+                border=1            #边框的格子宽度大小
             )
 
         qr.add_data(data)
